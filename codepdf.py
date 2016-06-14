9f9b41a9dbad5e978c06bb7cc3c2a6aef6279691
Christopher Welborn <cj@welbornprod.com>
2016-06-14 17:04:12 -0500

    stdin bug fix, add permalink ids.

--- codepdf.py
+++ codepdf.py
@@ -61,7 +61,7 @@ except ImportError as eximpcolr:
 colr_auto_disable()
 
 NAME = 'CodePDF'
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 VERSIONSTR = '{} v. {}'.format(NAME, VERSION)
 SCRIPT = os.path.split(os.path.abspath(sys.argv[0]))[1]
 SCRIPTDIR = os.path.abspath(sys.path[0])
@@ -201,6 +201,8 @@ def convert_files(
     styledefs = []
     for i, filename in enumerate(filenames):
         titletext = title or os.path.split(filename)[-1]
+        if titletext in (STDIN_NAME,):
+            titletext = 'stdin'
         formatter = get_formatter(
             stylename=stylename,
             linenos=linenos,
@@ -241,9 +243,14 @@ def convert_highlighted(filename, formatter):
     displayname, content = get_file_content(filename)
     lexer = get_file_lexer(filename, content)
     debug('Highlighting: {}'.format(displayname))
+    linkid = get_elem_id(displayname)
     return '\n'.join((
         '<div class="file">',
-        '<h2>{}</h2>'.format(displayname),
+        get_permalink_html(linkid),
+        '<h2 id="{}" style="display: inline-block">{}</h2>'.format(
+            linkid,
+            displayname
+        ),
         '<div class="{}">'.format(DIV_CLASS),
         highlight(content, lexer, formatter),
         '</div>',
@@ -330,6 +337,16 @@ def debug(*args, **kwargs):
     print(*pargs, **kwargs)
 
 
+def get_elem_id(s):
+    """ Transform a file name or text into a slug, usable for an element id.
+        Removes non alpha-numeric characters, replaces spaces with -.
+    """
+    return '-'.join(
+        ''.join(c for c in word if c.isalnum())
+        for word in s.split()
+    ).lower()
+
+
 def get_file_content(filename):
     """ Returns a tuple of (display_name, content), handling stdin if
         STDIN_NAME is used.
@@ -382,6 +399,25 @@ def get_formatter(stylename=None, linenos=False, title=None, full=False):
     return formatter
 
 
+def get_permalink_html(linkid):
+    """ Return HTML needed to build a permalink link/icon for a header. """
+    svg = """
+    <svg style="vertical-align: middle; display: inline;"
+    height="16" version="1.1" viewBox="0 0 16 16" width="16">
+    <path d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0
+    1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98
+    0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98
+    0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31
+    7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z">
+    </path></svg>
+    """
+    return '\n'.join((
+        '<a href="#{}" style="text-decoration: none;">'.format(linkid),
+        svg,
+        '</a>'
+    ))
+
+
 def get_output_name(filenames, output_name=None, html_mode=False):
     """ Determine output file name to use when the user hasn't given one. """
     if output_name:
@@ -420,7 +456,7 @@ def print_styles():
 
 def read_stdin():
     """ Read from stdin, print a message if it's a terminal. """
-    if syd.stdin.isatty() and sys.stdout.isatty():
+    if sys.stdin.isatty() and sys.stdout.isatty():
         print('\nReading from stdin until end of file (Ctrl + D)...\n')
     return sys.stdin.read()
 
